ba22681d8bc7caa2f7c0d9bc05a369714a344151
nahjjun <nahjjun1125@naver.com>
2026-08-06 17:25:58 +0900

    :wrench: Settings: RestTemplateConfig - 식품안전청 API 동기 호출용 RestTemplate 설정 Class - Connection Timeout : 5초 제한 - 데이터 Read Timeout: 10초 제한

---
+++ src/main/java/com/likelion/routineeatbe/global/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.likelion.routineeatbe.global.config;
+
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(5));
+        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+        return new RestTemplate(requestFactory);
+    }
+}

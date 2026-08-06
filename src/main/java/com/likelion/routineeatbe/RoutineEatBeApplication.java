b416641ee57e7f94df4141fd684babd4601cfa7d
nahjjun <nahjjun1125@naver.com>
2026-08-06 17:55:14 +0900

    :bug: Fix: BaseTimeEntity 설정 시 빠진 속성 추가

--- src/main/java/com/likelion/routineeatbe/RoutineEatBeApplication.java
+++ src/main/java/com/likelion/routineeatbe/RoutineEatBeApplication.java
@@ -2,8 +2,10 @@ package com.likelion.routineeatbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RoutineEatBeApplication {
 
     public static void main(String[] args) {

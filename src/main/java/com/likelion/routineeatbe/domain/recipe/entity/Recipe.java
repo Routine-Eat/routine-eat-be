ad1142e0d7bdb795755509af9e0e8c74c67c4db5
nahjjun <nahjjun1125@naver.com>
2026-08-06 18:05:20 +0900

    :sparkles: Feat: Recipe, RecipeStep Entity와 Enum 설정

---
+++ src/main/java/com/likelion/routineeatbe/domain/recipe/entity/Recipe.java
@@ -0,0 +1,57 @@
+package com.likelion.routineeatbe.domain.recipe.entity;
+
+import com.likelion.routineeatbe.domain.menu.entity.Menu;
+import com.likelion.routineeatbe.domain.recipe.enums.RecipeType;
+import com.likelion.routineeatbe.global.common.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "recipe")
+public class Recipe extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RecipeType type;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "recipe", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RecipeStep> recipeSteps = new ArrayList<>();
+
+    public static Recipe createBasic(Menu menu) {
+        return Recipe.builder()
+                .type(RecipeType.BASIC)
+                .menu(menu)
+                .build();
+    }
+}
---
+++ src/main/java/com/likelion/routineeatbe/domain/recipe/enums/RecipeStepType.java
@@ -0,0 +1,6 @@
+package com.likelion.routineeatbe.domain.recipe.enums;
+
+public enum RecipeStepType {
+    NORMAL,
+    CAUTION
+}

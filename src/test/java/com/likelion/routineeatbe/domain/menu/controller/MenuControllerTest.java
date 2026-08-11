fa2788f76b121738f03108d13eea6848c32ef47f
nahjjun <nahjjun1125@naver.com>
2026-08-11 13:23:12 +0900

    :white_check_box: Test: 테스트 추가

--- src/test/java/com/likelion/routineeatbe/domain/menu/controller/MenuControllerTest.java
+++ src/test/java/com/likelion/routineeatbe/domain/menu/controller/MenuControllerTest.java
@@ -5,8 +5,10 @@ import static org.mockito.BDDMockito.given;
 
 import com.likelion.routineeatbe.domain.menu.dto.response.InitMenuAndRecipeCookingEquipmentResDto;
 import com.likelion.routineeatbe.domain.menu.dto.response.InitMenuAndRecipeFoodIngredientResDto;
+import com.likelion.routineeatbe.domain.menu.dto.response.InitMenuDifficultyLevelResDto;
 import com.likelion.routineeatbe.domain.menu.service.InitMenuAndRecipeCookingEquipmentService;
 import com.likelion.routineeatbe.domain.menu.service.InitMenuAndRecipeFoodIngredientService;
+import com.likelion.routineeatbe.domain.menu.service.InitMenuDifficultyLevelService;
 import com.likelion.routineeatbe.domain.menu.service.MenuAndRecipeCrawlingService;
 import com.likelion.routineeatbe.global.response.GlobalResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -32,6 +34,9 @@ class MenuControllerTest {
 
     @Mock
     private InitMenuAndRecipeCookingEquipmentService initCookingEquipmentService;
+
+    @Mock
+    private InitMenuDifficultyLevelService initMenuDifficultyLevelService;
 
     @Test
     @DisplayName("메뉴 음식 재료 초기화 API가 201과 초기화 개수를 반환한다")
@@ -72,5 +77,25 @@ class MenuControllerTest {
         assertThat(response.getBody().getMessage())
                 .isEqualTo("메뉴/레시피별 필요 조리 도구 데이터 초기화를 성공했습니다.");
         assertThat(response.getBody().getData().initCount()).isEqualTo(1100L);
+    }
+
+    @Test
+    @DisplayName("메뉴 난이도 초기화 API가 201과 초기화 개수를 반환한다")
+    void 메뉴_난이도_초기화_API_201_반환_성공() {
+        // given
+        given(initMenuDifficultyLevelService.initialize())
+                .willReturn(InitMenuDifficultyLevelResDto.create(1156L));
+
+        // when
+        ResponseEntity<GlobalResponse<InitMenuDifficultyLevelResDto>> response =
+                menuController.initMenuDifficultyLevels();
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().isSuccess()).isTrue();
+        assertThat(response.getBody().getCode()).isEqualTo(201);
+        assertThat(response.getBody().getMessage()).isEqualTo("성공했습니다.");
+        assertThat(response.getBody().getData().initCount()).isEqualTo(1156L);
     }
 }

38958bf5cc0ccc08b6ac4a4519c417495b9d8a57
Jeongmokjin <jungmokjin@gmail.com>
2026-08-11 14:39:26 +0900

    ✨ Feat: 사용자-식재료 관계 삭제/수정 API 추가 (#18)
    * ✨ Feat : 사용자-식재료 관계 삭제 API 추가
    
    * 🗃️ Comment : 컨트롤러독스 스웨거 명세 수정
    
    * ✨ Feat: 사용자 식재료 보유량 수정 API 추가

--- src/main/java/com/likelion/routineeatbe/domain/user/controller/UserController.java
+++ src/main/java/com/likelion/routineeatbe/domain/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.likelion.routineeatbe.domain.user.controller;
 
 import com.likelion.routineeatbe.domain.user.dto.request.CreateUserFoodIngredientRequest;
 import com.likelion.routineeatbe.domain.user.dto.request.CreateUserRequest;
+import com.likelion.routineeatbe.domain.user.dto.request.DeleteUserFoodIngredientRequest;
+import com.likelion.routineeatbe.domain.user.dto.request.UpdateOwnFoodIngredientAmountRequest;
 import com.likelion.routineeatbe.domain.user.dto.response.UserFoodIngredientResponse;
 import com.likelion.routineeatbe.domain.user.dto.response.UserResponse;
 import com.likelion.routineeatbe.domain.user.entity.UserFoodIngredientType;
@@ -45,5 +47,17 @@ public class UserController implements UserControllerDocs {
             CreateUserFoodIngredientRequest createUserFoodIngredientRequest){
         UserFoodIngredientResponse userFoodIngredientResponse=userFoodIngredientService.createUserFoodIngredient(userId,createUserFoodIngredientRequest);
         return GlobalResponse.success("사용자-식재료 관게 추가에 성공했습니다.",userFoodIngredientResponse);
+    }
+
+    @Override
+    public GlobalResponse<UserFoodIngredientResponse> updateOwnFoodIngredientAmount(Long userId, UpdateOwnFoodIngredientAmountRequest request){
+        UserFoodIngredientResponse userFoodIngredientResponse=userFoodIngredientService.updateOwnFoodIngredientAmount(userId,request);
+        return GlobalResponse.success("식재료 보유량이 성공적으로 수정되었습니다.",userFoodIngredientResponse);
+    }
+
+    @Override
+    public GlobalResponse deleteUserFoodIngredient(Long userId, DeleteUserFoodIngredientRequest request){
+        userFoodIngredientService.deleteUserFoodIngredient(userId,request);
+        return GlobalResponse.success("사용자-식재료 관계가 성공적으로 삭제되었습니다.");
     }
 }

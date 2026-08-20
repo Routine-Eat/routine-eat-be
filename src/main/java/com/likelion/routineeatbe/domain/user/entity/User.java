bd67f235da8a056137a58453a0dd2def682141e1
Jeong mokjin <jungmokjin@gmail.com>
2026-08-21 00:27:02 +0900

    ✨ Feat: 사용자 삭제 API 추가

--- src/main/java/com/likelion/routineeatbe/domain/user/entity/User.java
+++ src/main/java/com/likelion/routineeatbe/domain/user/entity/User.java
@@ -1,8 +1,19 @@
 package com.likelion.routineeatbe.domain.user.entity;
 
+import com.likelion.routineeatbe.domain.cookingRecord.entity.CookingRecord;
+import com.likelion.routineeatbe.domain.favoriteRecipe.entity.FavoriteRecipe;
+import com.likelion.routineeatbe.domain.mealPlan.entity.MealPlan;
+import com.likelion.routineeatbe.domain.notification.entity.Notification;
+import com.likelion.routineeatbe.domain.userSearchHistory.entity.UserSearchHistory;
+import com.likelion.routineeatbe.domain.userStatistics.entity.UserStatistics;
+import com.likelion.routineeatbe.domain.userStatistics.entity.UserStatisticsFoodIngredient;
+import com.likelion.routineeatbe.domain.userStatistics.entity.UserStatisticsRecipe;
 import com.likelion.routineeatbe.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,6 +33,38 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = true)
     private SkillLevel skillLevel;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<UserFoodIngredient> userFoodIngredients = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<UserCookingEquipment> userCookingEquipments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<UserSearchHistory> userSearchHistories  = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<UserStatistics> userStatistics = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<FavoriteRecipe> favoriteRecipes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<Notification> notifications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<MealPlan> mealPlans = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<CookingRecord> cookingRecords = new ArrayList<>();
 
     public static User createUser(String loginNumber){
         return User.builder()
